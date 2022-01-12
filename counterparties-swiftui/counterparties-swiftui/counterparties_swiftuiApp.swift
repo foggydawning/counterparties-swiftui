3f4c546d834c8eddb0c24291c21a7a739f012bc3
foggydawning <foggydawning@gmail.com>
2022-01-12 11:26:36 +0300

    create xcode project

---
+++ counterparties-swiftui/counterparties-swiftui/counterparties_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterparties_swiftuiApp.swift
+//  counterparties-swiftui
+//
+//  Created by Илья Чуб on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct counterparties_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
